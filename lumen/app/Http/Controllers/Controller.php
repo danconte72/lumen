a47ea2036bab469187156bb7165e0d451f1efdb6
Pedro Henrique P <pedroxhp@outlook.com>
2018-09-22 15:09:34 -0300

    Update Controller.php

--- lumen/app/Http/Controllers/Controller.php
+++ lumen/app/Http/Controllers/Controller.php
@@ -8,13 +8,3 @@ class Controller extends BaseController
 {
     //
 }
-
-$router->get("listarporid/{id}", "AuditController@listarPorId");
-
-$router->get("listarTodasPerguntas", "AuditController@listarTodasPerguntas");
-
-$router->get("responder/{idPergunta}/{idResposta}", "AuditController@responder");
-
-$router->get("responderTodas/{arrayPerguntas}/{arrayRespostas}", "AuditController@responderTodas");
-
-$router->get("calcular/{arrayPerguntas}/{arrayRespostas}", "AuditController@calcular");
