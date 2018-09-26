8efc5f91b146495892c9db30b782c560431f5209
Guilherme Tavares <guilhermetavares13@hotmail.com>
2018-09-26 20:53:13 -0300

    merge

--- lumen/routes/web.php
+++ lumen/routes/web.php
@@ -15,29 +15,29 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-// $router->post("chat/{protocolo}/mensagens", "MensagensController@enviar");
+$router->post("chat/{protocolo}/mensagens", "MensagensController@enviar");
 
-// $router->get("chat/{protocolo}/mensagens" , "MensagensController@listar");
+$router->get("chat/{protocolo}/mensagens" , "MensagensController@listar");
 
-// $router->get("chat", "ChatController@criarChat");
+$router->get("chat", "ChatController@criarChat");
 
-// $router->get("chat/{protocolo}/finalizar", "ChatController@finalizarChat");
+$router->get("chat/{protocolo}/finalizar", "ChatController@finalizarChat");
 
-// $router->get("chat/{protocolo}/atendente", "ChatController@insereAtendente");
+$router->get("chat/{protocolo}/atendente", "ChatController@insereAtendente");
 
-// $router->get("cadastrarVisitante[/{nome}][/{telefone}][/{sexo}][/{idade}][/{idLocal}]", "VisitanteController@CadastrarVisitante");
+$router->get("cadastrarVisitante[/{nome}][/{telefone}][/{sexo}][/{idade}][/{idLocal}]", "VisitanteController@CadastrarVisitante");
 $router->get("cadastrarVisitante[/{nome}/{telefone}/{sexo}/{idade}/{idLocal}]", "VisitanteController@cadastrarVisitante");
 
-// $router->get("listarporid/{id}", "AuditController@listarPorId");
+$router->get("listarporid/{id}", "AuditController@listarPorId");
 
-// $router->get("listarTodasPerguntas", "AuditController@listarTodasPerguntas");
+$router->get("listarTodasPerguntas", "AuditController@listarTodasPerguntas");
 
-// $router->get("responder/{idPergunta}/{idResposta}", "AuditController@responder");
+$router->get("responder/{idPergunta}/{idResposta}", "AuditController@responder");
 
-// $router->get("responderTodas/{arrayPerguntas}/{arrayRespostas}", "AuditController@responderTodas");
+$router->get("responderTodas/{arrayPerguntas}/{arrayRespostas}", "AuditController@responderTodas");
 
-// $router->get("calcular/{arrayPerguntas}/{arrayRespostas}", "AuditController@calcular");
+$router->get("calcular/{arrayPerguntas}/{arrayRespostas}", "AuditController@calcular");
 
-// $router->get("cadastrarUsuario/{nome}/{eMail}/{sexo}/{telefone}/{CPF}/{DataNasc}}", "UsuarioController@cadastarUsuario");
+$router->get("cadastrarUsuario/{nome}/{eMail}/{sexo}/{telefone}/{CPF}/{DataNasc}}", "UsuarioController@cadastarUsuario");
 
 $router->get("cadastrarLocal/{cidade}/{bairro}", "LocalController@cadastrarLocal");
