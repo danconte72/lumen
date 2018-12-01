b688df5867199f3780758db93e59d9eb58af416a
Pedro Henrique Paulek <pedroxhp@outlook.com>
2018-12-01 11:01:57 -0200

    commit-011118_06

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -19,7 +19,7 @@ class UsuarioController extends BaseController
         //         'id' => "|".$nome."|".$eMail."|".$login."|".$senha."|".$sexo."|".$telefone."|".$CPF."|".$DataNasc
         //     ]);
         // return app('db')->select("SELECT idUsuario as id FROM usuario");
-        $query = "INSERT INTO usuario (nome, email, login, senha, sexo, telefone, cpf, datanasc) VALUES (";
+        $query = "INSERT INTO usuario (idusuario, nome, email, login, senha, sexo, telefone, cpf, datanasc) VALUES (";
         $query += "'" . $idusuario . "','" . $nome . "','" . $email . "','" . $login . "','" . $senha . "'," . $sexo . ",'" . $telefone . "','" . $cpf . "','" . $datanasc ."');";
         $query += "SELECT currval(pg_get_serial_sequence('usuario','idusuario')) as id";
         return app('db')->select($query);
