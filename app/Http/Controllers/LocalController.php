2c090dbadf8bae2d4e5545737e3a3d3880888b95
Pedro Henrique Paulek <pedroxhp@outlook.com>
2018-12-01 14:56:14 -0200

    commit-011118_12

--- app/Http/Controllers/LocalController.php
+++ app/Http/Controllers/LocalController.php
@@ -17,10 +17,15 @@ class LocalController extends BaseController
         //         'id' => "1"
         //     ]);
         // return app('db')->select("SELECT idLocal as id FROM local;");
-        $query = "INSERT INTO local (cidade, bairro) VALUES (";
-        $query += "'" . $cidade . "','" . $bairro ."');";
+            // $query = "INSERT INTO local (cidade, bairro) VALUES (";
+            // $query += "'" . $cidade . "','" . $bairro . "');";
+        // $query = "INSERT INTO local (cidade, bairro) VALUES ("'" . $cidade . "','" . $bairro . "');
+        // $query += "SELECT currval(pg_get_serial_sequence('local','idLocal')) as id;";
+
+        $query = "INSERT INTO local VALUES ('$cidade[cidade]','$bairro[bairro]')";
         $query += "SELECT currval(pg_get_serial_sequence('local','idLocal')) as id;";
         return app('db')->select($query);
+        
         // SELECT currval(pg_get_serial_sequence('local','idLocal')) as id; -> Example.
     }
     public function listarLocal () 
