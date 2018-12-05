44bcc5bbdfe109843eca527abcc697344c5f2922
Daniel Antonio Conte <danconte72@gmail.com>
2018-12-05 19:42:58 -0200

    teste

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -23,7 +23,7 @@ class UsuarioController extends BaseController
         // $query += "'" . $nome . "','" . $email . "','" ;
         // $query += $login . "','" . $senha . "'," . $sexo . ",'" . $telefone . "','" . $cpf . "','" . $datanasc ."');";
         $query = "INSERT INTO ENTORPECENTE (NOME) VALUES('loló');";
-        $query += "SELECT currval(pg_get_serial_sequence('entorpecente','identorpecente')) as id";
+        $query .= "SELECT currval(pg_get_serial_sequence('entorpecente','identorpecente')) as id";
         return app('db')->select($query);
     }
     public function listarUsuarios () 
