41a9951e399aa8af0b3434554f74a20f6584fac8
Daniel Antonio Conte <danconte72@gmail.com>
2018-12-05 19:49:42 -0200

    teste

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -19,10 +19,9 @@ class UsuarioController extends BaseController
         //         'id' => "|".$nome."|".$email."|".$login."|".$senha."|".$sexo."|".$telefone."|".$cpf."|".$datanasc
         //     ]);
         // return app('db')->select("SELECT idUsuario as id FROM usuario");
-        // $query = "INSERT INTO usuario (nome, email, login, senha, sexo, telefone, cpf, datanasc) VALUES (";
-        // $query += "'" . $nome . "','" . $email . "','" ;
-        // $query += $login . "','" . $senha . "'," . $sexo . ",'" . $telefone . "','" . $cpf . "','" . $datanasc ."');";
-        $query = "INSERT INTO ENTORPECENTE (NOME) VALUES ('loló');";
+        $query = "INSERT INTO usuario (nome, email, login, senha, sexo, telefone, cpf, datanasc) VALUES (";
+        $query .= "'" . $nome . "','" . $email . "','" ;
+        $query .= $login . "','" . $senha . "'," . $sexo . ",'" . $telefone . "','" . $cpf . "','" . $datanasc ."');";
         // $query .= "SELECT currval(pg_get_serial_sequence('entorpecente','identorpecente')) as id";
         return app('db')->select($query);
     }
