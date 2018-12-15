ba3550758d86bb103bdc3811f201540f125430ef
Pedro Henrique Paulek <pedroxhp@outlook.com>
2018-12-15 21:14:18 -0200

    commit-151218_03

--- app/Http/Controllers/LocalController.php
+++ app/Http/Controllers/LocalController.php
@@ -17,19 +17,6 @@ class LocalController extends BaseController
     {
         return app('db')->select("SELECT idLocal as id, cidade, bairro FROM local;");
     }
-    
-    public function cadastrarUsuario ($cidade,$bairro,$idLocal=null)
-    {
-        if ($idLocal == null) {
-            $query = "INSERT INTO local (cidade, bairro) VALUES (";
-            $query .= "'" . $cidade . "','" . $bairro . "');";
-            // $query = "SELECT currval(pg_get_serial_sequence('local','idLocal')) as id";
-            return app('db')->select($query);
-        } else {
-            $query = "UPDATE local SET cidade = '" . $cidade . "', bairro = '" . $bairro . "' WHERE idLocal = " . $idLocal . ";";
-            return app('db')->select($query);
-        }
-    }
 
     public function cadastrarLocal ($cidade,$bairro,$idLocal=null)
     {
